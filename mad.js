9a6c00f54f69fe63666b582b3841a3dd163dd571
genomeprjct <genemoeprjctpogo@gmail.com>
2020-08-31 23:42:52 -0400

    Setting up energy

--- mad.js
+++ mad.js
@@ -3,6 +3,7 @@ const ditto = q => q.rewards_string == '3 Rare Candies' && q.conditions_string==
 const dragon = q => q.rewards_string == '3 Rare Candies' && q.conditions_string=='Catch 1 Pokémon (Type: Dragon)'
 const dragonDust = q => q.conditions_string=='Catch 1 Pokémon (Type: Dragon)'
 const d4d = q => q.rewards_string == 'Pokémon Encounter: Dratini' && q.conditions_string=='Catch 1 Pokémon (Type: Dragon)'
+const truth = q => true;
 const axios = require('axios')
 const sorter = require('./utils').sorter;
 
@@ -22,6 +23,15 @@ const mad = function(message, args) {
     } else if (param == 'dragon-dust') {
         nycurl = 'https://nycpokemap.com/quests.php?quests%5B%5D=7%2C0%2C290&quests%5B%5D=3%2C1500%2C0&quests%5B%5D=3%2C1000%2C0'
         filter = dragonDust
+    } else if (param == 'venusaur-energy') {
+        nycurl = 'https://sgpokemap.com/quests.php?quests%5B%5D=7%2C0%2C290&quests%5B%5D=12%2C0%2C3&time=1598931609000'
+        filter = truth;
+    } else if (param == 'blastoise-energy') {
+        nycurl = 'https://sgpokemap.com/quests.php?quests%5B%5D=7%2C0%2C290&quests%5B%5D=12%2C0%2C9&time=1598931665855'
+        filter = truth;
+    } else if (param == 'charizard-energy') {
+        nycurl = 'https://sgpokemap.com/quests.php?quests%5B%5D=7%2C0%2C290&quests%5B%5D=12%2C0%2C6&time=1598931698392'
+        filter = truth;
     }
 
     axios.get(nycurl)
